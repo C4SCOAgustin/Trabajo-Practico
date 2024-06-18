5f9e18a758b1efa2312aa8bca925b8d99ce8dbf4
antunez578 <agustinaantunez138@gmail.com>
2024-06-18 18:20:36 -0300

    retorno las imagenes de la api con la funcion getAllImages() en Home, como me pide mostrar las dos listas, entonces hago una sola variable con las dos listas vacias y llamo a la funcion auxiliar en la funcion getAllImages  utilizo la variable json_collection para llamar a las imagenes de la api. recorro la lista  json yl las agrego a la lista images.

--- nasa_image_gallery/layers/services/services_nasa_image_gallery.py
+++ nasa_image_gallery/layers/services/services_nasa_image_gallery.py
@@ -9,8 +9,10 @@ def getAllImages(input=None):
     # obtiene un listado de imágenes desde transport.py y lo guarda en un json_collection.
     # ¡OJO! el parámetro 'input' indica si se debe buscar por un valor introducido en el buscador.
     json_collection = []
-
     images = []
+    json_collection= transport.getAllImages(input)
+    for foto in json_collection:
+        images.append(mapper.fromRequestIntoNASACard(foto))
 
     # recorre el listado de objetos JSON, lo transforma en una NASACard y lo agrega en el listado de images. Ayuda: ver mapper.py.
 
